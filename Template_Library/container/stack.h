c5c24eb66514e59c568421d4d7b3ea07498c587f
Dennis Schulze <dennis@axelschulze.net>
2019-08-20 20:14:41 +0200

    stack: added move assignment & ctor for rvalues tuple: added primitive implementation

--- Template_Library/container/stack.h
+++ Template_Library/container/stack.h
@@ -11,102 +11,58 @@ namespace ds {
 	>
 	class stack {
 	public:
-		//using node_type = mono_node<Type>;
+		using value_type = typename Manager::value_type;
 		using node_type = typename Manager::node_type;
 
-		//using iterator = iterators::traverse_iterator<Type>;
 		using iterator = typename Manager::iterator;
 
 	private:
 		size_t num_elements = 0;
 		Manager data;
 
-		// OLD IMPLEMENTATION
-		//size_t current_size = 10;
-		//node_type* head = nullptr;
-		//std::unique_ptr<node_type[]> elements = std::make_unique<node_type[]>( current_size );
-
 	public:
 		stack() = default;
 
-		stack( const Type& t ) {
-			// OLD IMPLEMENTATION
-			//elements[0].value = t;
-			//head = &elements[0];
+		stack( const value_type& t ) {
 			data.insert( t );
 			++num_elements;
 		}
 
+		stack( value_type&& val ) {
+			data.insert( std::forward<value_type>(val) );
+			++num_elements;
+		}
+
 		stack( stack&& st ) {
-			// OLD IMPLEMENTATION
-			//head = st.head;
-			//st.head = nullptr;
-			//elements = std::move( st.elements );
-			//current_size = st.current_size;
 			data = std::move(st.data);
 			num_elements = st.num_elements;
+		}
+
+		stack& operator = ( stack&& st ) {
+			data = std::move( st.data );
+			num_elements = st.num_elements;
+			return *this;
 		}
 
 		stack(const stack& st) = delete;
 		stack& operator = ( const stack& ) = delete;
 
 
-		void push( const Type& t ) {
-			// OLD IMPLEMENTATION
-			//if ( num_elements < current_size ) {
-			//
-			//	// finde freien Platz
-			//	for ( unsigned int i = 1; i < this->current_size; i++ ) {
-			//		if ( elements[i] == node_type() ) {
-			//			elements[i] = node_type(t, head);
-			//			head = &elements[i];
-			//
-			//			break;
-			//		}
-			//	}
-			//
-			//}
-			//else {
-			//
-			//	// Speicherzuweisung
-			//	auto temp_alloc = std::make_unique<node_type[]>( current_size + 10 );
-			//
-			//	for ( unsigned int i = 0; i < current_size; i++ ) {
-			//		temp_alloc[i] = elements[i];
-			//	}
-			//
-			//	elements = std::move( temp_alloc );
-			//	current_size += 10;
-			//
-			//	auto index = current_size - 10;
-			//
-			//	elements[index].value = t;
-			//	elements[index].next = head;
-			//	head = &elements[index];
-			//}
-
+		void push( const value_type& t ) {
 			data.insert( t );
-
 			++num_elements;
 		}
 
-		const Type& top() const {
-			// OLD IMPLEMENTATION
-			//if ( head != nullptr )
-			//	return head->value;
-			//else
-			//	return Type{};
+		void push( value_type&& t ) {
+			data.insert( std::forward<value_type>(t) );
+			++num_elements;
+		}
+
+		const value_type& top() const {
 			return data.top();
 		}
 
 		void pop() {
-			// OLD IMPLEMENTATION
-			//if ( head != nullptr ) {
-			//	node_type* new_head = head->next;
-			//	*head = node_type();
-			//	head = new_head;
-			//	--num_elements;
-			//}
 			data.pop();
 			--num_elements;
 		}
@@ -116,26 +72,18 @@ namespace ds {
 		}
 
 		iterator begin() const {
-			// OLD IMPLEMENTATION
-			//return iterator( head );
 			return data.begin();
 		}
 
 		iterator end() const {
-			// OLD IMPLEMENTATION
-			//return iterator();
 			return data.end();
 		}
 
 		iterator begin() {
-			// OLD IMPLEMENTATION
-			//return iterator( head );
 			return data.begin();
 		}
 
 		iterator end() {
-			// OLD IMPLEMENTATION
-			//return iterator();
 			return data.end();
 		}
 	};
